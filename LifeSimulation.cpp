75256db51e5ec4895d26183ee439d4efe343bd7e
Ervann-Garaffa <ervanngaraffa-botta@hotmail.fr>
2022-09-04 23:49:19 +0200

    display particle

--- LifeSimulation.cpp
+++ LifeSimulation.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
@@ -13,10 +14,11 @@ struct Particle
     sf::RectangleShape shape;
     sf::Color color;
 
-    Particle(sf::Color m_color)
-        : shape(sf::Vector2f(PARTICLE_SIZE, PARTICLE_SIZE)), color(m_color)
+    Particle(sf::Color m_color, float m_x, float m_y)
+        : shape(sf::Vector2f(PARTICLE_SIZE, PARTICLE_SIZE)), pos(sf::Vector2f(m_x, m_y)), color(m_color)
     {
         shape.setFillColor(color);
+        shape.setPosition(pos);
     }
 
     void update() {}
@@ -31,6 +33,10 @@ int main()
     frame.setOutlineThickness(1);
     frame.setOutlineColor(sf::Color::Blue);
     frame.setFillColor(sf::Color::Black);
+    frame.setPosition(window.getSize().x - 650, window.getSize().y - 650);
+
+    std::vector<Particle> particles;
+    particles.emplace_back(sf::Color::Red, 150, 300);
 
     while (window.isOpen())
     {
@@ -39,14 +45,13 @@ int main()
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-                window.close();
-            
-            frame.setPosition(window.getSize().x - 650, window.getSize().y - 650);
-
-            window.clear(sf::Color::Black);
-            window.draw(frame);
-            window.display();
+                window.close();    
         }
+        
+        window.clear(sf::Color::Black);
+        particles[0].render(window);
+        window.draw(frame);
+        window.display();
     }
 
     return EXIT_SUCCESS;
