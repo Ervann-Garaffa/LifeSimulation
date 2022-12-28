5ff829a2342cf6085359ef220743e05f9a294caa
Ervann-Garaffa <ervanngaraffa-botta@hotmail.fr>
2022-12-28 17:01:14 +0100

    Font resource + dynamic sizing

--- main.cpp
+++ main.cpp
@@ -1,26 +1,63 @@
+#include <iostream>
+#include <math.h>
 #include <string>
 #include <vector>
 
 #include <SFML/Graphics.hpp>
 
+const int WINDOW_WIDTH = 2000;
+const int WINDOW_HEIGHT = 1000;
+const int WORLD_SIZE = std::min(WINDOW_WIDTH, WINDOW_HEIGHT) - 200;
+const sf::Vector2f WORLD_ORIGIN = sf::Vector2f(WINDOW_WIDTH - WORLD_SIZE - 100, 100);
+const int GRID_SCALE = WORLD_SIZE / 10; // Always grid 10 x 10
+
 struct Particle
 {
-    sf::Vector2f position;
-    sf::Vector2f speed;
-    sf::Vector2f acceleration;
+    sf::Vector2f m_position;
+    sf::Vector2f m_speed;
+    sf::Vector2f m_acceleration;
+    int m_radius;
+    sf::Color m_color;
+    sf::CircleShape m_shape;
+
+    Particle()
+    :   m_position(120.f, 799.f), m_speed(0.f, 0.f), m_acceleration(0.f, 0.f), m_radius(10.f), m_color(sf::Color::Red)
+    {
+        m_shape.setRadius(50.f);
+        m_shape.setPosition(m_position);
+        m_shape.setFillColor(m_color);
+    }
+
+    ~Particle(){}
+
+    // TODO: update and shape display functions
+};
+
+// Hash function enabling choice of grid slot AKA table position
+const int hashFunction(Particle* p_particle)
+{
+    return (WORLD_SIZE / GRID_SCALE) * std::floor(p_particle->m_position.y / GRID_SCALE) + std::floor(p_particle->m_position.x / GRID_SCALE);
+}
+
+// TODO: create table of linked lists of particles and visualization grid. Table is 800 x 800, grid 10 x 10
+struct HashTable
+{
     
-    sf::CircleShape shape;
-    sf::Vector2f radius;
-    sf::Color color;
-
-
 };
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1600, 800), "SFML works!");
-    Particle* particle = new Particle;
+    // Window size matters, grid visualization of Hash Table and World Area both at 800 x 800
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML works!");
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf")){ std::cout << "Couldn't load font from file";}
 
+    sf::RectangleShape worldArea(sf::Vector2f(WORLD_SIZE, WORLD_SIZE));
+    worldArea.setFillColor(sf::Color::Black);
+    worldArea.setPosition(WORLD_ORIGIN);
+
+    Particle particle;
+    
     // Main window loop
     while (window.isOpen())
     {
@@ -29,8 +66,9 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
 
-        window.clear();
-        //window.draw(a.m_shape);
+        window.clear(sf::Color(0x222222FF));
+        window.draw(worldArea);
+        window.draw(particle.m_shape);
         window.display();
     }
 
